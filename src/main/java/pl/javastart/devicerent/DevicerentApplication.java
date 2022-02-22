b969c2ad8280c970dded8e211521bcf97d2149d7
Krystian <krystian14d@gmail.com>
2022-02-22 17:32:43 +0100

    initial commit

---
+++ src/main/java/pl/javastart/devicerent/DevicerentApplication.java
@@ -0,0 +1,13 @@
+package pl.javastart.devicerent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevicerentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevicerentApplication.class, args);
+    }
+
+}
